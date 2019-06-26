682762bc230a1aa4396b17f5fb82c6f709ac227b
Geovanny Alzate Sandoval <vany0114@hotmail.com>
2019-06-25 22:09:38 -0500

    sample code

---
+++ dbcontextfactory_healthcheck/CustomEFHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dbcontextfactory_healthcheck
+{
+    // TODO: we can make this guy generic.
+    public class CustomEFHealthCheck : IHealthCheck
+    {
+        private readonly Func<MyDbContext> _dbContextFactory;
+        
+        public CustomEFHealthCheck(Func<MyDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            using (var dbContext = _dbContextFactory())
+            {
+                if (await dbContext.Database.CanConnectAsync(cancellationToken))
+                {
+                    return HealthCheckResult.Healthy();
+                }
+
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
---
+++ dbcontextfactory_healthcheck/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dbcontextfactory_healthcheck
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+    }
+}
